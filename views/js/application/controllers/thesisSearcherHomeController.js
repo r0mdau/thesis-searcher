b012122da1dbe2b3ecfd92fb8142af3776cde26d
Romain Dauby <romain.dauby@gmail.com>
2014-05-14 12:44:30 +0200

    Retour des infos du service rest sur écran

--- views/js/application/controllers/thesisSearcherHomeController.js
+++ views/js/application/controllers/thesisSearcherHomeController.js
@@ -3,7 +3,6 @@ thesisSearcherControllers.controller('thesisSearcherHomeController', ['$scope', 
       $scope.query = '';
 
       $scope.$watch('query', function() {
-         console.log($scope.query);
-         $scope.answers = PDF.get({searchValue: $scope.query});
+         $scope.answers = PDF.get({searchValue: $scope.query});         
      });
   }]);
