90ae14e882b4d855d13b4904a9189893a93675da
Romain Dauby <romain.dauby@gmail.com>
2014-05-15 18:38:40 +0200

    Contrat d'interface API defini Issue #2 @Adrael tu peux commencer a bosser la vue avec ce mock, les datas seront definitivement envoyees sous ce format Issue #1 in progress

--- thesis-searcher.js
+++ thesis-searcher.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var elasticsearch = require('elasticsearch');
-var client = new elasticsearch.Client();
  
 var app = express();
 
@@ -8,13 +7,53 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
-});
+})
 
-app.get('/pdf', function(req, res) {
-    res.send({message:'Veuillez fournir un paramètre de recherche'});
-});
-app.get('/pdf/:id', function(req, res) {
-    res.send({id:req.params.id, name: "The Name", description: "description"});
-});
- 
-app.listen(80);
+.get('/info', function(req, res){
+	res.send({data : {
+                info:'Plus d\'informations conernant le contrat d\'interface de cette API ici : https://github.com/r0mdau/thesis-searcher/blob/master/README.md'}
+            });
+})
+
+.get('/search', function(req, res) {
+    res.send({data : {
+				warning:'Veuillez fournir un element de recherche'}
+			});
+})
+.get('/search/:id', function(req, res) {
+	// :id = req.params.id
+    res.send({data : 
+				[{
+					date	: "14 Avril 2013", 
+					author	: "Romain Dauby", 
+					fileName: "M13_DAUBY.pdf"
+				},
+				{
+                    date    : "24 Juin 2013",
+                    author  : "Felix Wattez",
+                    fileName: "M13_WATTEZ.pdf"
+                }]
+			});
+})
+
+.get('/pdf', function(req, res) {
+    res.send({data : {
+				warning:'Veuillez fournir l\'identifiant du PDF demande'}
+			});
+})
+.get('/pdf/:id', function(req, res) {
+	res.send({data :
+                {
+                    date    : "14 Avril 2013",
+                    author  : "Romain Dauby",
+                    fileName: "M13_DAUBY.pdf",
+                    content : "WAZAAAAAAAAA CONTENU DE OUF"
+                }
+            });
+})
+
+.use(function(req, res, next){
+    res.redirect('/info');
+}) 
+
+.listen(80);
