fcad9ff92ea575706b78c2e838b227f6a2f736b7
Romain Dauby <romain.dauby@gmail.com>
2014-05-19 19:00:08 +0200

    Recherche dans le contenu du pdf #6

--- thesis-searcher.js
+++ thesis-searcher.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client();
  
 var app = express();
 
@@ -20,22 +21,28 @@ app.use(function(req, res, next) {
 				warning:'Veuillez fournir un element de recherche'}
 			});
 })
-.get('/search/:id', function(req, res) {
-	// :id = req.params.id
-    res.send({data : 
-				[{
-					id		: "1",
-					date	: "14 Avril 2013", 
-					author	: "Romain Dauby", 
-					fileName: "M13_DAUBY.pdf"
-				},
-				{
-					id		: "2",
-                    date    : "24 Juin 2013",
-                    author  : "Felix Wattez",
-                    fileName: "M13_WATTEZ.pdf"
-                }]
-			});
+
+.get('/search/:queryString', function(req, res) {
+	var datas = {data : []};
+	client.search({
+		index: 'thesis',
+		q: 'content:'+req.params.queryString
+	}, function (error, response) {
+		if(response.hits){
+			if(response.hits.hits){
+				var memoires = response.hits.hits;
+				for(var i = 0; i < memoires.length; i++){
+					datas.data.push({
+						id : memoires[i]._source.id,
+						date : memoires[i]._source.date,
+						author : memoires[i]._source.author,
+						fileName : memoires[i]._source.fileName
+					});
+				}
+			}
+		}
+		res.send(datas);
+	});
 })
 
 .get('/pdf', function(req, res) {
