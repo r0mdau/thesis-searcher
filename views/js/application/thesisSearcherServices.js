b0547e8e39d5dfa69ef58d2fe58f2a01a7a2a07d
Raphaël MARQUES <work.rmarques@gmail.com>
2014-05-28 09:34:50 +0200

    Variabilzing IP Address #12

--- views/js/application/thesisSearcherServices.js
+++ views/js/application/thesisSearcherServices.js
@@ -1,13 +1,22 @@
 var thesisSearcherServices = angular.module('thesisSearcherServices', ['ngResource']);
+
+thesisSearcherServices.factory('IPProvider', [
+  function(){
+  	return {
+  		getServerIP: function() {
+    		return '178.32.102.161';
+  		}
+	}
+  }]);
  
-thesisSearcherServices.factory('PDF', ['$resource',
-  function($resource){
-    return $resource('http://178.32.102.161/search/:searchValue', {}, {});
+thesisSearcherServices.factory('PDF', ['$resource', 'IPProvider',
+  function($resource, IPProvider){
+    return $resource('http://' + IPProvider.getServerIP() + '/search/:searchValue', {}, {});
   }]);
 
-thesisSearcherServices.factory('PDFDetail', ['$resource',
-  function($resource){
-    return $resource('http://178.32.102.161/pdf/:searchValue', {}, {});
+thesisSearcherServices.factory('PDFDetail', ['$resource', 'IPProvider',
+  function($resource, IPProvider){
+    return $resource('http://' + IPProvider.getServerIP() + '/pdf/:searchValue', {}, {});
   }]);
 
 thesisSearcherServices.factory('GlobalStorage', [ '$rootScope',
